0c916d251a1a9f99cdcbb635abc294062959b308
Jan Lindgren <jhlindgren@gmail.com>
2020-04-04 11:32:00 +0200

    create user model

--- app/User.php
+++ app/User.php
@@ -2,13 +2,30 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Support\Str;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    /**
+     * Set a UUID on create.
+     */
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $unique = false;
+            do {
+                $uuid = Str::uuid();
+                if (Self::where('uuid', $uuid)->count() == 0) {
+                    $unique = true;
+                }
+            } while (!$unique);
+    
+            $model->uuid = $uuid;
+        });
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -16,24 +33,11 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
+        'dob',
+        'gender',
+        'city',
+        'occupation',
+        'long',
+        'lat',
     ];
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->uuid('uuid');
+            $table->date('dob')->nullable()->index();
+            $table->string('gender', 16)->index();
+            $table->string('city', 64)->nullable()->index();
+            $table->string('occupation', 128)->nullable();
+            $table->decimal('long', 10, 7)->nullable();
+            $table->decimal('lat', 10, 7)->nullable();
             $table->timestamps();
         });
     }
